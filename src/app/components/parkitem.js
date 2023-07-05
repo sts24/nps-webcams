b122e5a6d4510c8a0bd76f182453ef7daf62b090
Scott Smith <scottsmith24@gmail.com>
2023-07-04 19:28:09 -0700

    toggle active class

--- src/app/components/parkitem.js
+++ src/app/components/parkitem.js
@@ -1,12 +1,17 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 import styles from '@/app/styles/park-item.module.scss'
 
 export default function ParkItem ({ data }) {
 
+    const router = useRouter()
+    const slug = router.query.slug !== undefined ? router.query.slug.join() : null
+    const linkIsActive = `/${slug}` == data.slug ? styles['parkItemLink--active'] : ''
+
     return (
         <li className={styles.parkItem}>
-            <Link href={data.slug} className={styles.parkItemLink}>{data.name}</Link>
+            <Link href={data.slug} className={`${styles.parkItemLink} ${linkIsActive}`}>{data.name}</Link>
         </li>
     )
 
